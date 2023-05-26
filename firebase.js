fe96057f02d31b650a91c846790397955796c5ee
alejandromedina1 <alejandro.medina1@u.icesi.edu.co>
2023-05-25 19:56:27 -0500

    Funciona el login y el sign-up

--- firebase.js
+++ firebase.js
@@ -8,8 +8,19 @@ import {
     getDocs,
     addDoc,
     setDoc,
-    doc
+    doc,
 } from "firebase/firestore";
+import {
+    getStorage,
+    ref,
+    uploadBytes,
+    getDownloadURL
+} from "firebase/storage";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -26,6 +37,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
 
 export async function getTasks() {
 
@@ -53,9 +66,70 @@ export async function addTask(taskTitle) {
     }
 }
 
+export async function addUserToDb(userInfo, id) {
+    try {
+        await setDoc(doc(db, "users", id), userInfo);
+        console.log("user written with ID: ", id);
+    } catch (e) {
+        console.error("Error adding user: ", e);
+    }
+}
+
 export async function editDocument(title, id) {
     await setDoc(doc(db, "Tasks", id), {
         title: title,
         completed: true,
     });
+}
+
+export async function createUser(userInfo) {
+    try {
+        //Sign up
+        const userCredential = await createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password)
+        const user = userCredential.user
+        console.log(user)
+
+        //Subir Imagen
+        const url = await uploadFile(user.id + userInfo.picture.name, userInfo.picture, 'profilePicture')
+
+        //Crear usuario en DB
+        const dbInfo = {
+            url,
+            email: userInfo.email,
+            birthday: userInfo.birthday,
+            username: userInfo.username
+        }
+        addUserToDb(dbInfo, user.id)
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(error.message)
+        // ..
+    }
+}
+
+export async function uploadFile(name, file, folder) {
+    const taskImgRef = ref(storage, `${folder}/${name}`);
+
+    try {
+        await uploadBytes(taskImgRef, file);
+        const url = await getDownloadURL(taskImgRef);
+        return url;
+    } catch (error) {
+        console.log("error creando imagen ->", error);
+    }
+}
+
+export async function logInUser(email, password) {
+
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password)
+        const user = userCredential.user;
+        return {status: true, info: user.id}
+
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        return {status: false, info: errorMessage}
+    };
 }
